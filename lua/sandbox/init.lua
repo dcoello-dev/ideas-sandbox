d4fbc131e24b3f718fd57663ab23ba740b5f824e
dcoello-dev <dcpulido91@gmail.com>
2024-06-17 23:52:07 +0200

    Split lua functionality in several files
    Signed-off-by: dcoello-dev <dcpulido91@gmail.com>

--- lua/sandbox/init.lua
+++ lua/sandbox/init.lua
@@ -1,12 +1,9 @@
+local ut = require("sandbox.utils")
+
 local M = {}
 
-function script_path()
-  local str = debug.getinfo(2, "S").source:sub(2)
-  return str:match("(.*/)")
-end
-
 local default_config = { storage_path = "~/.local/share/nvim/lazy/sandbox/ideas/" }
-local framework_path = script_path() .. "../../framework/scripts/framework.py"
+local framework_path = ut.script_path() .. "../../framework/scripts/framework.py"
 
 function M.setup(options)
   if options then
@@ -17,16 +14,8 @@ function M.setup(options)
   vim.keymap.set('n', '<leader>gw', M.sandbox)
   vim.keymap.set('n', '<leader>ge', M.errors)
   vim.keymap.set('n', '<leader>gl', M.load)
-  vim.keymap.set('n', '<leader>gm', string.format("<cmd>edit %s/../../main.*<CR>", script_path()))
+  vim.keymap.set('n', '<leader>gm', string.format("<cmd>edit %s/../../main.*<CR>", ut.script_path()))
   vim.keymap.set('n', '<leader>gc', "<cmd>cclose<CR><cmd>ToggleTerm<CR>")
-end
-
-function M.format()
-  vim.cmd("%!clang-format")
-end
-
-function M.execute(cmd)
-  vim.cmd(string.format("2TermExec direction=vertical display_name=exe size=80 cmd='%s'", cmd)) 
 end
 
 function M.errors()
@@ -34,24 +23,16 @@ function M.errors()
   vim.cmd("copen")
 end
 
-function M.run()
-  M.execute("./a.out")
-  M.execute("rm ./a.out 2> /dev/null")
+function M.sandbox()
+  require(string.format("sandbox.%s_support", vim.bo.filetype)).pipeline()
 end
 
-function M.sandbox ()
-  M.format()
-  vim.cmd("w")
-  M.execute("g++ % 2>&1 | tee output")
-  M.run()
-end
-
-function M.reset (opts)
-  M.execute(string.format("python3 %s --reset --lang %s --storage %s", framework_path , opts, M.options.storage_path))
+function M.reset(opts)
+  ut.execute(string.format("python3 %s --reset --lang %s --storage %s", framework_path , opts, M.options.storage_path))
 end
 
 function M.save(opts)
-  M.execute(string.format("python3 %s --save --storage %s", framework_path, M.options.storage_path))
+  ut.execute(string.format("python3 %s --save --storage %s", framework_path, M.options.storage_path))
 end
 
 function M.load(opts)
